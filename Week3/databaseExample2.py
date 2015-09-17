4f615d668ee844453f7a5891ac378f48537dfbb2
Patrick M. Dudas <dudaspm@gmail.com>
2015-09-17 12:05:30 -0400

    Create databaseExample2.py

---
+++ Week3/databaseExample2.py
@@ -0,0 +1,146 @@
+################################
+### Connects to Database
+### Gets Data from Database
+################################
+
+import sqlite3 as lite
+import sys
+import os 
+
+
+con = None
+
+
+directoryForDB = "C:/Users/Pudders/Desktop/DBClass/" + "carsDatabase.db"
+
+con = lite.connect(directoryForDB)
+############################
+## Reminder!
+## "CREATE TABLE Cars(Id INT, Name TEXT, Price INT)"
+###########################
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])
+		
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT Name FROM Cars')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s""" % (row[0])
+
+############################
+## Distinct
+###########################	
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT DISTINCT(Price) FROM Cars')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s""" % (row[0])
+
+############################
+## Where
+###########################		
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars WHERE Price > 40000')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])
+
+############################
+## Between
+###########################			
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars WHERE Price between 10000 and 40000')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])		
+	
+############################
+## Like/Wildcard
+###########################			
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars WHERE Name LIKE "Vo%"')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])	
+
+############################
+## Order 
+###########################			
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars ORDER BY Name')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])
+		
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars ORDER BY Name ASC')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])
+
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT * FROM Cars ORDER BY Name DESC')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s %s""" % (row[0], row[1], row[2])		
+	
+############################
+## Count 
+###########################	
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT Count(*) FROM Cars')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s""" % (row[0])		
+		
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT Count(*) FROM Cars WHERE Price between 10000 and 40000')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s""" % (row[0])			
+############################
+## Sum 
+###########################	
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT SUM(Price) FROM Cars')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s""" % (row[0])	
+############################
+## Max 
+###########################	
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT MAX(Price) FROM Cars')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s""" % (row[0])
+		
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT Name, MAX(Price) FROM Cars;')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s""" % (row[0], row[1])	
+
+with con:
+	cur = con.cursor()    
+	cur.execute('SELECT Name, Price FROM Cars WHERE Price = (SELECT MAX(Price) FROM Cars);')
+	rows = cur.fetchall()
+	#for row in rows:    
+	#	print """%s %s""" % (row[0], row[1])			
