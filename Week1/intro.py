01eb9196e4c55d48a656c575bfb5feccb5875fd8
Ken <xynoci.k@gmail.com>
2015-10-18 17:25:26 -0400

    Add comments to intro.py.

--- Week1/intro.py
+++ Week1/intro.py
@@ -7,14 +7,23 @@
 #####################################################
 
 variable = "hello"
-print variable
+print variable 
+## hello
 type(variable)
+## <type 'str'>
+
 
 variable = 12
 type(variable)
+## <type 'int'>
 
 variable = {"apple","banana"}
 type(variable) 
+## <type 'set'>
+
+variable = ['1','1','1']
+type(variable)
+## <type 'list'>
 
 variable = "some string"
 print variable
@@ -25,13 +34,33 @@ variable[0:5]
 variable[:5]
 variable[5:]
 
-
+## Return the lowest index in the string where substring sub is found, such that sub is contained in the slice s[start:end].(Case sensitive)
 print variable.find("me")
+print variable.find("mE")
+
+## Return a copy of the string with its first character capitalized and the rest lowercased.
 print variable.capitalize()
+
+## No this attribute (lowercase).
+print 'HEllo'.lowercase()
+
+## Return the highest index in the string where substring sub is found (first index of the last instance in the string), such that sub is contained within s[start:end]. 
+print 'Hello hllo'.rfind()
+## 7
+
+## Return a copy of the string with trailing characters removed. The chars argument is a string specifying the set of characters to be removed. If omitted or None, the chars argument defaults to removing whitespace. The chars argument is not a suffix; rather, all combinations of its values are stripped.
+print 'mississippi'.rstrip('ipz')
+## mississ
+
+## Return a copy of the string with all occurrences of substring old replaced by new. If the optional argument count is given, only the first count occurrences are replaced.
+print 'mississippi'.replace('iss',' ',1)
+## m issippi
+
 
 numberVariable = 42
 print "the answer to life, universe, and everything is.. " + `numberVariable`
 print "the answer to life, universe, and everything is.. " + str(numberVariable)
+## showes no difference.
 
 ##############################################################################
 #
@@ -114,6 +143,7 @@ while (count < len(city)):
 ########### Section 2 - Arrays/Lists ##############
 #####################################################
 
+## index starts from 0
 arrayOfNumbers = [0,1,2,3,4,5,6]
 print arrayOfNumbers[1]
 
@@ -128,6 +158,7 @@ pals.append("Sally")
 pals.append("Joe")
 
 print pals
+## ['Glenn', 'Sally', 'Joe']
 print type(pals)
 
 for friend in pals:
@@ -149,6 +180,19 @@ pal["email"] = "glenng@some.edu"
 pal["phone"] = "123-123-1221"
 
 print pal
+## {'phone': '123-123-1221', 'last': 'Golden', 'email': 'glenng@some.edu', 'first': 'Glenn'}
+print pal.viewkeys()
+## dict_keys(['phone', 'last', 'email', 'first'])
+print pal.keys()
+## ['phone', 'last', 'email', 'first']
+print pal.viewvalues()
+## dict_values(['123-123-1221', 'Golden', 'glenng@some.edu', 'Glenn'])
+print pal.values()
+## ['123-123-1221', 'Golden', 'glenng@some.edu', 'Glenn']
+type(pal.values())
+## <type 'list'>
+
+
 print type(pal)
 for contactInfo in pal:
 	print contactInfo, pal[contactInfo]
